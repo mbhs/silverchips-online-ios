6a76816a29562b9a7b9eb0cfed87d4adb7065182
Nonameentered <matthewshu88@gmail.com>
2020-04-04 01:21:37 -0400

    Initial Commit

---
+++ Silverchips Online/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Silverchips Online
+//
+//  Created by Matthew on 4/4/20.
+//  Copyright © 2020 Technaplex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
